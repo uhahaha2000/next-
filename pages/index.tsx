527012a9f422084b38bdf0d06c67efef25db7f07
uhahaha2000 <43199041+uhahaha2000@users.noreply.github.com>
2023-05-06 19:49:52 +0900

    modified:   pages/index.tsx 	modified:   styles/_add.scss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,63 @@
 import "../styles/Home.scss";
 import Head from "next/head";
-import { useState, ChangeEvent } from "react";
+import React, { useState, useEffect, useRef, ChangeEvent } from "react";
 
 interface TextAreaProps {
-  onInput: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+  onChange: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+  value: string;
 }
 
 export default function Home(props: TextAreaProps) {
-  const [textareaHeight, setTextareaHeight] = useState({
-    row: 1,
-    lineBreak: {},
-  });
-  const resizeTextarea = (e) => {
-    const { scrollHeight, clientHeight, value } = e.target;
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const fakeInputRef = useRef<HTMLDivElement | null>(null);
 
-    // 줄바꿈이 일어날 때
-    if (scrollHeight > clientHeight) {
-      setTextareaHeight((prev) => ({
-        row: prev.row + 1,
-        lineBreak: { ...prev.lineBreak, [value.length - 1]: true },
-      }));
-    }
+  const [value, setValue] = useState<String>("입력해주세요.");
 
-    // 텍스트 지워서 줄바꿈 지점에 도달했을 때
-    if (textareaHeight.lineBreak[value.length]) {
-      setTextareaHeight((prev) => ({
-        row: prev.row - 1,
-        lineBreak: { ...prev.lineBreak, [value.length]: false },
-      }));
-    }
+  const textAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(event.target.value);
   };
-  const onKeyEnter = (e) => {
-    if (e.code === "Enter") {
-      setTextareaHeight((prev) => ({
-        row: prev.row + 1,
-        lineBreak: { ...prev.lineBreak, [5]: true },
-      }));
+
+  const fakeInputHeight = fakeInputRef.current?.clientHeight;
+
+  useEffect(() => {
+    if (textareaRef && textareaRef.current) {
+      textareaRef.current.style.height = fakeInputHeight + "px";
+      const scrollHeight = textareaRef.current.scrollHeight;
+
+      textareaRef.current.style.height = scrollHeight + "px";
     }
+    console.log(
+      textareaRef.current?.scrollHeight,
+      textareaRef.current?.clientHeight
+    );
+  }, [value]);
+
+  const handleBlur = () => {
+    if (textareaRef && textareaRef.current) {
+      textareaRef.current.style.height = fakeInputHeight + "px";
+    }
+    console.log(
+      textareaRef.current?.scrollHeight,
+      textareaRef.current?.clientHeight
+    );
   };
+
+  const handleFocus = () => {
+    if (textareaRef && textareaRef.current) {
+      if (
+        textareaRef.current.scrollHeight >
+        textareaRef.current.clientHeight + 3
+      ) {
+        textareaRef.current.style.height =
+          textareaRef.current.scrollHeight + "px";
+      }
+    }
+    console.log(
+      textareaRef.current?.scrollHeight,
+      textareaRef.current?.style.lineHeight
+    );
+  };
+
   return (
     <>
       <Head>
@@ -46,38 +66,36 @@ export default function Home(props: TextAreaProps) {
       </Head>
 
       <div className="wrap">
-        <div className="head">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </div>
         <div className="scroll">
-          <div className="inner">가나다라마바</div>
+          <div className="inner">
+            가나다라마바<a href="https://nextjs.org"></a>
+          </div>
         </div>
 
-        <div className="user_space_wrap">
-          <div className="recommends_question_wrap opened">
-            {/* opened */}
-            <div className="recommend_list_wrap">
-              여기는 링크리스트 나오는 부분~~~ 여기는 링크리스트 나오는 부분~~~
-              여기는 링크리스트 나오는 부분~~~ 여기는 링크리스트 나오는 부분~~~
-              여기는 링크리스트 나오는 부분~~~ 여기는 링크리스트 나오는 부분~~~
-              여기는 링크리스트 나오는 부분~~~
-            </div>
-          </div>
+        <div className="user_enter_wrap">
           <div className="typing_wrap">
-            <button className="btn_recommends" type="button">
+            <button className="btn_menu" type="button">
               버튼
             </button>
             <div className="chat_input_wrap">
               <div className="chat_input_inner">
-                <div className="fake_input"></div>
                 <textarea
                   className="chat_input"
-                  placeholder="검색해보세요."
                   autoComplete="off"
-                  onInput={resizeTextarea}
-                  onKeyDown={onKeyEnter}
-                  rows={textareaHeight.row}
+                  placeholder="입력해주세요."
+                  ref={textareaRef}
+                  onChange={textAreaChange}
+                  onFocus={handleFocus}
+                  onBlur={handleBlur}
                 ></textarea>
+                <div
+                  className={
+                    "fake_input" + (value === "" ? " placeholder" : "")
+                  }
+                  ref={fakeInputRef}
+                >
+                  {value ? value : "입력해주세요."}
+                </div>
               </div>
               <button className="btn_send" disabled></button>
             </div>
